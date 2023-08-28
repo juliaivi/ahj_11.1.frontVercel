8cf094875cb43c5563a32ee8255b1c6c4cfaeb7d
juliaivi <juliashinobi@mail.ru>
2023-08-28 15:26:17 +0300

    change widget v1

--- src/js/Wiget.js
+++ src/js/Wiget.js
@@ -1,6 +1,6 @@
 import { ajax } from 'rxjs/ajax';
 import {
-  catchError, interval, of, take, map, concatMap,
+  catchError, interval, of, map, fromEvent, EMPTY, switchMap,
 } from 'rxjs';
 
 import creatMessage from './creatMessage';
@@ -9,8 +9,8 @@ import errorMessage from './errorMessage';
 export default class Widget {
   constructor() {
     this.container = document.querySelector('.container');
-    this.url = 'https://ahj-11-1-back-vercel-ziii.vercel.app/messages/unread';
-    // this.url = 'http://localhost:3000/messages/unread';
+    // this.url = 'https://ahj-11-1-backv2.onrender.com/messages/unread';
+    this.url = 'http://localhost:3000/messages/unread';
     this.subject = null;
     this.table = document.querySelector('table');
     this.tbody = document.querySelector('tbody');
@@ -19,7 +19,8 @@ export default class Widget {
 
   init() {
     this.tbody.addEventListener('click', (e) => this.onClick(e));
-    this.subscribeToUpdate();
+    this.btnClick$ = fromEvent(document.querySelector('.container'), 'click'); // fromEvent Создает наблюдаемый объект, который генерирует события определенного типа, исходящие из заданной цели события.
+    this.onClickBtn();
   }
 
   onClick(e) {
@@ -41,27 +42,50 @@ export default class Widget {
     }
   }
 
-  subscribeToUpdate() {
-    let data = null;
-    this.stream$ = interval(4000) // поток, через сколько нужно запускать
-      .pipe( // позволяет производить манипуляции с потоком
-        take(2), // получить 5 значений и поток завершится switchMap mergeMap
-        concatMap(() => ajax.getJSON(this.url).pipe(// получение значения из фетча пярмо в потоке
-          map((messages) => messages.messages), // достать значение чтоб в обзервере ничего не делать
-          catchError((error) => { // принимает колбек и на выход т.к. предыдущий поток прерван, может вернуть новый
-            console.log('error: ', error);
-            if (!this.table.querySelector('.error')) {
-              errorMessage();
-            }
+  subscribeStream() { // подписываемся на поток
+    return ajax.getJSON(this.url).pipe(// отправляем запрос
+      map((response) => response),
+      catchError((error) => { // принимает колбек и на выход т.к. предыдущий поток прерван, может вернуть новый
+        console.log('error: ', error);
+        if (!this.table.querySelector('.error')) {
+          errorMessage();
+        }
 
-            return of(error); // генерируе ответ сами, вывожу ошибку
-          }),
-        )),
-      )
-      .subscribe((res) => { // подписаться на данный поток, получить некий результат (получить данные)
-        data = res;
+        return of(); // генерируе ответ сам, можно выводить ошибку
+      }),
+    );
+  }
+
+  onClickBtn() {
+    let subscribe = false;
+    this.startClick$ = this.btnClick$.pipe( // создание потока когда щелкнула по нему. Меняет состояние кнопки и статуса
+      map((e) => {
+        const elem = e.target;
+        if (elem.classList.contains('btn')) {
+          if (elem.classList.contains('btn-subscribe')) {
+            elem.classList.remove('btn-subscribe');
+            elem.classList.add('btn-unsubscribe');
+            elem.textContent = 'Отписаться';
+            subscribe = true;
+          } else {
+            elem.classList.add('btn-subscribe');
+            elem.classList.remove('btn-unsubscribe');
+            elem.textContent = 'Подписаться';
+            subscribe = false;
+          }
+        }
+        return subscribe;
+      }),
+    );
+
+    this.startClick$.pipe( // еще так можно останавливать поток takeUntil(this.stream$)
+      switchMap((isStart) => (isStart ? interval(2000) : EMPTY)), // Простое наблюдаемое, которое выдает только полное уведомление. Его можно использовать для компоновки с другими наблюдаемыми, например, в mergeMap.
+      // Простой наблюдаемый объект, который не передает наблюдателю никаких элементов и немедленно отправляет полное уведомление.
+      switchMap(() => this.subscribeStream()),
+    )
+      .subscribe((response) => { // подписаться на данный поток, получить некий результат (получить данные)
+        const data = response.messages;
         if (data.length > 0) {
-          this.tbody.replaceChildren();
           data.forEach((elem) => {
             if (elem.subject.length > 15) {
               this.shortenText(elem.subject);
@@ -74,7 +98,7 @@ export default class Widget {
       });
   }
 
-  // проверяет длину текста
+  // сокращение слова
   shortenText(el) {
     this.subject = `${el.substr(0, 15)}...`;
   }
